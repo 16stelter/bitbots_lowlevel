043fbfa0d0936309ec94dc1037a8057007c34ce0
16stelter <6stelter@informatik.uni-hamburg.de>
2022-04-22 11:16:07 +0200

    Adds reading from hall sensor

--- bitbots_ros_control/include/bitbots_ros_control/hall_hardware_interface.h
+++ bitbots_ros_control/include/bitbots_ros_control/hall_hardware_interface.h
@@ -1,5 +1,5 @@
-#ifndef BITBOTS_ROS_CONTROL_INCLUDE_BITBOTS_ROS_CONTROL_BITFOOT_HARDWARE_INTERFACE_H_
-#define BITBOTS_ROS_CONTROL_INCLUDE_BITBOTS_ROS_CONTROL_BITFOOT_HARDWARE_INTERFACE_H_
+#ifndef BITBOTS_ROS_CONTROL_INCLUDE_BITBOTS_ROS_CONTROL_HALL_HARDWARE_INTERFACE_H_
+#define BITBOTS_ROS_CONTROL_INCLUDE_BITBOTS_ROS_CONTROL_HALL_HARDWARE_INTERFACE_H_
 
 #include <rclcpp/rclcpp.hpp>
 #include <string>
@@ -16,8 +16,24 @@ namespace bitbots_ros_control {
                                            int id,
                                            std::string topic_name,
                                            std::string name);
+
+            bool init();
+
+            void read(const rclcpp::Time &t, const rclcpp::Duration &dt);
+
+            void write(const rclcpp::Time &t, const rclcpp::Duration &dt);
         private:
-    }
+            rclcpp::Node::SharedPtr nh_;
+            std::shared_ptr<DynamixelDriver> driver_;
+            int id_;
+            std::string topic_name_;
+            std::string name_;
+            uint8_t *data_;
+
+            std::vector<float> current_angle_;
+            bitbots_msgs::msg::FloatStamped msg_;
+            rclcpp::Publisher<bitbots_msgs::msg::FloatStamped>::SharedPtr angle_pub_; //TODO: define angle message
+    };
 }
 
 #endif
